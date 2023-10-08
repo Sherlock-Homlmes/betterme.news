fff584684d67a563129b064a27437ac518b0dd4c
Ta Minh Khoi <khoitm@codenet.jp>
2023-10-08 14:52:45 +0700

    fix: python precommit not work

--- others/setup.sh
+++ others/setup.sh
@@ -2,12 +2,11 @@ docker compose build
 docker compose run --rm server poetry install
 docker compose run --rm client npm install
 
-git config --unset-all core.hooksPath
+bun install
 
 python3 -m venv .venv
 source ./.venv/bin/activate
 pip install -r requirements.txt
 cd others
+git config --unset-all core.hooksPath
 pre-commit install
-
-bun install
