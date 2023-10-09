924d8c12a9944c6f2ab31b77f733a76e190d0935
Ta Minh Khoi <khoitm@codenet.jp>
2023-10-10 02:57:17 +0700

    fix: setup bash file

--- others/setup.sh
+++ others/setup.sh
@@ -1,8 +1,14 @@
-docker compose build
-docker compose run --rm server poetry install
-docker compose run --rm admin-server poetry install
-docker compose run --rm client npm install
+# build docker and install package
+docker-compose build
+docker-compose run --rm server poetry install
+docker-compose run --rm admin-server poetry install
+docker-compose run --rm client npm install
 
+# create .env file from example.env file
+cp server/example.env server/.env
+cp admin_server/example.env server/.env
+
+# install common tools
 bun install
 
 python3 -m venv .venv
