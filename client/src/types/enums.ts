ae15f9dd315640ddcc44c86c6be1e75e7bd8c8fe
Ta Minh Khoi <khoitm@codenet.jp>
2023-10-16 00:30:48 +0700

    feat: ruff fix pre commit

--- client/src/types/enums.ts
+++ client/src/types/enums.ts
@@ -1,19 +1,19 @@
 // From export enums.py (3 Enums 9 Values)
 
 export enum ABCEnum2 {
-  abc1 = "abc1",
-  abc2 = "abc2",
-  abc3 = "abc3",
+   abc1 = 'abc1',
+   abc2 = 'abc2',
+   abc3 = 'abc3'
 }
 
 export enum SomethingEnum {
-  something1 = 1,
-  something2 = 2,
-  something3 = 3,
+   something1 = 1,
+   something2 = 2,
+   something3 = 3
 }
 
 export enum SomethingEnum2 {
-  something1 = "10",
-  something2 = "20",
-  something3 = "30",
+   something1 = '10',
+   something2 = '20',
+   something3 = '30'
 }
