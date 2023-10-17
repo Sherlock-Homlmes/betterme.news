7e35352b4cd3ae385de63b8e616b8b12f88c7d43
Ta Minh Khoi <khoitm@codenet.jp>
2023-10-17 20:46:22 +0700

    feat: deploy to koyeb

--- others/server-requirements.sh
+++ others/server-requirements.sh
@@ -1,1 +1,2 @@
-docker compose run --rm server poetry export --without-hashes --without-urls | awk '{ print $1 }' FS=';' > ./server/requirements.txt
+# docker compose run --rm server poetry export --without-hashes --without-urls | awk '{ print $1 }' FS=';' > ./server/requirements.txt
+docker compose run --rm server pip freeze > server/requirements.txt
