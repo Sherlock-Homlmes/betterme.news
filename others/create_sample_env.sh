76e19cb40c3e4e5b6bc8145540c6f3843cb1fc58
Ta Minh Khoi <khoitm@codenet.jp>
2023-10-23 00:23:14 +0700

    feat: test docker compose ci

---
+++ others/create_sample_env.sh
@@ -0,0 +1,11 @@
+# create .env file from example.env file if there are not exist .env file
+if [ ! -f ./server/.env ]; then
+    echo "Generate server .env file"
+    cp server/example.env server/.env
+fi
+if [ ! -f ./admin_server/.env ]; then
+    echo "Generate admin server .env file"
+    cp admin_server/example.env admin_server/.env
+fi
+
+echo "Create sample env done"
--- others/setup.sh
+++ others/setup.sh
@@ -4,15 +4,7 @@ docker compose run --rm server poetry install --only-root
 docker compose run --rm admin-server poetry install --only-root
 docker compose run --rm client npm install --no-save
 
-# create .env file from example.env file if there are not exist .env file
-if [ ! -f ./server/.env ]; then
-    echo "Generate server .env file"
-    cp server/example.env server/.env
-fi
-if [ ! -f ./admin_server/.env ]; then
-    echo "Generate admin server .env file"
-    cp admin_server/example.env admin_server/.env
-fi
+bash others/create_sample_env.sh
 
 # install common tools
 bun install
