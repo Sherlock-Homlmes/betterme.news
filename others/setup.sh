ebd163d97283d4e1240f44b7949f735e6d7a0d0b
Ta Minh Khoi <khoitm@codenet.jp>
2023-10-22 16:25:47 +0700

    fix: setup step and docker something fail

--- others/setup.sh
+++ others/setup.sh
@@ -1,8 +1,8 @@
 # build docker and install package
 docker compose build
-docker compose run --rm server poetry install
-docker compose run --rm admin-server poetry install
-docker compose run --rm client npm install
+docker compose run --rm server poetry install --only-root
+docker compose run --rm admin-server poetry install --only-root
+docker compose run --rm client npm install --no-save
 
 # create .env file from example.env file if there are not exist .env file
 if [ ! -f ./server/.env ]; then
