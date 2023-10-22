97f342636ca364432d8982d19d18025dccca6324
Ta Minh Khoi <khoitm@codenet.jp>
2023-10-23 01:02:08 +0700

    feat: test docker compose ci

---
+++ others/ci/install_server.sh
@@ -0,0 +1,6 @@
+# create sample .env file
+bash others/create_sample_env.sh
+pip install poetry
+# install dependencies
+cd server
+poetry install --only-root
--- others/ci/test_admin_server.sh
+++ others/ci/test_admin_server.sh
@@ -1,9 +1,3 @@
-# create sample .env file
-bash others/create_sample_env.sh
-pip install poetry
-# install dependencies
 cd admin_server
-poetry install --only-root
 # run test
-source .venv/bin/activate
 python -m pytest
