fd6e28a22ca4574759d77b01f696940cf17e44f9
Ta Minh Khoi <khoitm@codenet.jp>
2023-10-29 04:19:23 +0700

    feat: posts page

---
+++ client/src/components/Carousel.svelte
@@ -0,0 +1,66 @@
+<script>
+  import { Carousel } from "flowbite-svelte";
+
+  let index = 0;
+  const images = [
+    {
+      alt: "Câu Lạc Bộ Pháp Ngữ Học Viên Ngoại Giao Chính Thức Mở Đơn Tuyển Thành Viên",
+      src: "https://cdn4.ivolunteer.vn/post/wp-content/uploads/2023/10/27225259/Ban-sao-cua-Kich-thuoc-800x500px_Anh-Dai-Dien-Bai-Dang-Website-iVolunteer-14-3.png",
+      title: "cosmic-timetraveler-pYyOZ8q7AII-unsplash.com",
+    },
+    {
+      alt: "Câu Lạc Bộ Pháp Ngữ Học Viên Ngoại Giao Chính Thức Mở Đơn Tuyển Thành Viên",
+      src: "https://cdn4.ivolunteer.vn/post/wp-content/uploads/2023/10/27225259/Ban-sao-cua-Kich-thuoc-800x500px_Anh-Dai-Dien-Bai-Dang-Website-iVolunteer-14-3.png",
+      title: "cristina-gottardi-CSpjU6hYo_0-unsplash.com",
+    },
+    {
+      alt: "Câu Lạc Bộ Pháp Ngữ Học Viên Ngoại Giao Chính Thức Mở Đơn Tuyển Thành Viên",
+      src: "https://cdn4.ivolunteer.vn/post/wp-content/uploads/2023/10/27225259/Ban-sao-cua-Kich-thuoc-800x500px_Anh-Dai-Dien-Bai-Dang-Website-iVolunteer-14-3.png",
+      title: "johannes-plenio-RwHv7LgeC7s-unsplash.com",
+    },
+    {
+      alt: "Câu Lạc Bộ Pháp Ngữ Học Viên Ngoại Giao Chính Thức Mở Đơn Tuyển Thành Viên",
+      src: "https://cdn4.ivolunteer.vn/post/wp-content/uploads/2023/10/27225259/Ban-sao-cua-Kich-thuoc-800x500px_Anh-Dai-Dien-Bai-Dang-Website-iVolunteer-14-3.png",
+      title: "jonatan-pie-3l3RwQdHRHg-unsplash.com",
+    },
+    {
+      alt: "Câu Lạc Bộ Pháp Ngữ Học Viên Ngoại Giao Chính Thức Mở Đơn Tuyển Thành Viên",
+      src: "https://cdn4.ivolunteer.vn/post/wp-content/uploads/2023/10/27225259/Ban-sao-cua-Kich-thuoc-800x500px_Anh-Dai-Dien-Bai-Dang-Website-iVolunteer-14-3.png",
+      title: "mark-harpur-K2s_YE031CA-unsplash",
+    },
+    {
+      alt: "Câu Lạc Bộ Pháp Ngữ Học Viên Ngoại Giao Chính Thức Mở Đơn Tuyển Thành Viên",
+      src: "https://cdn4.ivolunteer.vn/post/wp-content/uploads/2023/10/27225259/Ban-sao-cua-Kich-thuoc-800x500px_Anh-Dai-Dien-Bai-Dang-Website-iVolunteer-14-3.png",
+      title: "pietro-de-grandi-T7K4aEPoGGk-unsplash",
+    },
+    {
+      alt: "Câu Lạc Bộ Pháp Ngữ Học Viên Ngoại Giao Chính Thức Mở Đơn Tuyển Thành Viên",
+      src: "https://cdn4.ivolunteer.vn/post/wp-content/uploads/2023/10/27225259/Ban-sao-cua-Kich-thuoc-800x500px_Anh-Dai-Dien-Bai-Dang-Website-iVolunteer-14-3.png",
+      title: "sergey-pesterev-tMvuB9se2uQ-unsplash",
+    },
+    {
+      alt: "Câu Lạc Bộ Pháp Ngữ Học Viên Ngoại Giao Chính Thức Mở Đơn Tuyển Thành Viên",
+      src: "https://cdn4.ivolunteer.vn/post/wp-content/uploads/2023/10/27225259/Ban-sao-cua-Kich-thuoc-800x500px_Anh-Dai-Dien-Bai-Dang-Website-iVolunteer-14-3.png",
+      title: "solotravelgoals-7kLufxYoqWk-unsplash",
+    },
+  ];
+  let image;
+</script>
+
+<div class="max-w-4xl space-y-4">
+  <Carousel
+    {images}
+    let:Indicators
+    let:Controls
+    on:change={({ detail }) => (image = detail)}
+  >
+    <Controls />
+    <Indicators />
+  </Carousel>
+
+  <div
+    class="rounded h-10 bg-gray-300 dark:bg-gray-700 dark:text-white p-2 my-2 text-center"
+  >
+    {image?.alt}
+  </div>
+</div>
---
+++ client/src/components/SearchInput.svelte
@@ -0,0 +1,55 @@
+<script>
+  import { Search, Button, Dropdown, DropdownItem } from "flowbite-svelte";
+  import "iconify-icon";
+
+  const items = [
+    {
+      label: "All categories",
+    },
+    {
+      label: "Mockups",
+    },
+    {
+      label: "Templates",
+    },
+    {
+      label: "Design",
+    },
+    {
+      label: "Logos",
+    },
+  ];
+
+  let selectCategory = "All categories";
+</script>
+
+<form class="flex w-[700px]">
+  <div class="relative">
+    <Button
+      class=" w-[150px] rounded-e-none whitespace-nowrap border border-r-0 border-primary-700"
+    >
+      {selectCategory}
+      <iconify-icon icon="mdi:chevron-down" class="w-2.5 h-2.5 ml-2.5" />
+    </Button>
+    <Dropdown classContainer="w-40">
+      {#each items as { label }}
+        <DropdownItem
+          on:click={() => {
+            selectCategory = label;
+          }}
+          class={selectCategory === label ? "underline" : ""}
+        >
+          {label}
+        </DropdownItem>
+      {/each}
+    </Dropdown>
+  </div>
+  <Search
+    size="md"
+    class="rounded-none py-2.5"
+    placeholder="Search Mockups, Logos, Design Templates..."
+  />
+  <Button class="!p-2.5 rounded-s-none">
+    <iconify-icon icon="mdi:magnify" class="w-5 h-5" />
+  </Button>
+</form>
