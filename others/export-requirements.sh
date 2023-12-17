41fc2e2b1b71ae8c9ab17f3313c881bee3848d66
LinuxDragon <linuxdragon@pop-os.localdomain>
2023-12-17 21:22:59 +0700

    fix: new design + fix bash + ruff formatter

--- others/export-requirements.sh
+++ others/export-requirements.sh
@@ -1,2 +1,2 @@
-docker-compose run --rm server pip freeze > server/requirements.txt
-docker-compose run --rm admin-server pip freeze > admin_server/requirements.txt
+docker compose run --rm server pip freeze > server/requirements.txt
+docker compose run --rm admin-server pip freeze > admin_server/requirements.txt
