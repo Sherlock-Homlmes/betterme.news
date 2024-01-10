ebf55b64c98668a085b2179fa1b2fd9ef25465a1
LinuxDragon <linuxdragon@pop-os.localdomain>
2024-01-10 23:57:46 +0700

    feat: can fix title in admin ui

--- admin_client/pages/something.vue
+++ admin_client/pages/something.vue
@@ -1,7 +1,7 @@
 <template>
-<center class="mb-15">
-    <div v-if="pageInfo">
-        <h1>{{ pageInfo?.title }}</h1>
+<center class="mt-5 mb-15">
+    <div v-if="pageInfo" class="w-75">
+        <v-text-field label="Title" v-model="pageInfo.title"></v-text-field>
         <img :src="`http://localhost/api/media/${pageInfo?.banner}`" >
         <v-file-input clearable label="File input" variant="solo-filled" class="w-50" ></v-file-input>
         <v-autocomplete
@@ -13,24 +13,8 @@
         multiple
         variant="solo-filled"
         ></v-autocomplete>
-        <v-textarea class="w-75" label="Description" variant="solo-filled" v-model="pageInfo.description"></v-textarea>
-        <v-card class="w-75" >
-            <v-tabs
-            v-model="tab"
-            color="blue"
-            align-tabs="center"
-            >
-                <v-tab :value="1">Web</v-tab>
-                <v-tab :value="2">Discord</v-tab>
-            </v-tabs>
-            <v-window v-model="tab">
-
-                <!-- html -->
-                <v-window-item
-                    :key="1"
-                    :value="1"
-                >
-                    <v-container fluid>
+        <v-textarea label="Description" variant="solo-filled" v-model="pageInfo.description"></v-textarea>
+        <v-card>
                         <editor
                             :init="{
                                 height: 1000,
@@ -49,56 +33,39 @@
                             initial-value="Loading..."
                             output-format="html"
                         />
-                    </v-container>
-                </v-window-item>
-
-                <!-- discord -->
-                <v-window-item
-                    :key="2"
-                    :value="2"
-                >
-                    <v-container fluid>
-                        <center class="mt-5">
-                            <v-btn prepend-icon="$vuetify"  @click="onDiscordPreview">Send test post</v-btn>
-                        </center>
-                    </v-container>
-                </v-window-item>
-            </v-window>
         </v-card>
-<v-stepper :items="['Step 1', 'Step 2', 'Step 3']" class="w-75 mt-10">
-  <template v-slot:item.1>
-     <v-card title="Chỉnh sửa nội dung" flat></v-card>
-  </template>
+        <v-stepper :items="['Step 1', 'Step 2', 'Step 3']" class="mt-10">
+        <template v-slot:item.1>
+            <v-card title="Chỉnh sửa nội dung" flat></v-card>
+        </template>
 
-  <template v-slot:item.2>
-    <v-card title="Xem trước nội dung đăng lên web" flat></v-card>
-                        <center class="my-5" :color='"green"'>
-                            <v-btn prepend-icon="$vuetify" @click="onHtmlPreview">
-                                      <template v-slot:prepend v-if="isHtmlPreviewed">
-        <v-icon color="success"></v-icon>
-      </template>
-                                Preview</v-btn>
-                        </center>
-    <v-card title="Xem trước bài đăng lên discord" flat></v-card>
-                        <center class="my-5">
-                            <v-btn prepend-icon="$vuetify"  @click="onDiscordPreview">
-                                      <template v-slot:prepend v-if="isDiscordPreviewed">
-        <v-icon color="success"></v-icon>
-      </template>
-                                Send</v-btn>
-                        </center>
-  </template>
+        <template v-slot:item.2>
+            <v-card title="Xem trước nội dung đăng lên web" flat></v-card>
+                                <center class="my-5" :color='"green"'>
+                                    <v-btn prepend-icon="$vuetify" @click="onHtmlPreview">
+                                            <template v-slot:prepend v-if="isHtmlPreviewed">
+                <v-icon color="success"></v-icon>
+            </template>
+                                        Preview</v-btn>
+                                </center>
+            <v-card title="Xem trước bài đăng lên discord" flat></v-card>
+                                <center class="my-5">
+                                    <v-btn prepend-icon="$vuetify"  @click="onDiscordPreview">
+                                            <template v-slot:prepend v-if="isDiscordPreviewed">
+                <v-icon color="success"></v-icon>
+            </template>
+                                        Send</v-btn>
+                                </center>
+        </template>
 
-  <template v-slot:item.3>
-    <v-card title="Tạo bài viết mới" flat></v-card>
-                        <center class="my-5">
-                            <v-btn prepend-icon="$vuetify" :disabled="!canSave">Create</v-btn>
-                        </center>
-  </template>
-</v-stepper>
+        <template v-slot:item.3>
+            <v-card title="Tạo bài viết mới" flat></v-card>
+                                <center class="my-5">
+                                    <v-btn prepend-icon="$vuetify" :disabled="!canSave">Create</v-btn>
+                                </center>
+        </template>
+        </v-stepper>
     </div>
-
-
     <v-progress-circular v-else indeterminate color="green" ></v-progress-circular>
 </center>
 </template>
@@ -124,7 +91,7 @@ const getPageInfo = async (link: string) => {
 
 const onDiscordPreview = ()=>{
     isDiscordPreviewed.value = true
-    console.log(pageInfo.value.description)
+    console.log(pageInfo.value)
 }
 const onHtmlPreview = ()=>{
     isHtmlPreviewed.value = true
