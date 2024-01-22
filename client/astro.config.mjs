7193be4551a85bffa6a9bc427946dffc9e0dca0d
LinuxDragon <linuxdragon@pop-os.localdomain>
2024-01-22 23:18:03 +0700

    fix: remove reading time to deploy cloudflare

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx'
 import cloudflare from '@astrojs/cloudflare'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +10,6 @@ export default defineConfig({
 	adapter: cloudflare(),
 	site: 'https://betterme.news/', // Write here your website url
 	markdown: {
-		remarkPlugins: [remarkReadingTime],
 		drafts: true,
 		shikiConfig: {
 			theme: 'material-theme-palenight',
--- client/src/data/site.config.ts
+++ client/src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
+	author: 'Little Rookie', // Site author
+	title: 'Betterme.news', // Site title.
+	description: 'Trang web cung cấp thông tin về  câu lạc bộ, tình nguyện,...', // Description to display in the meta tags
+	lang: 'vi',
+	ogLocale: 'vi',
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
