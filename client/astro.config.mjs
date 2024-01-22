1bbf4fc09fbccebc689e8d046bfacd82856d74a3
LinuxDragon <linuxdragon@pop-os.localdomain>
2024-01-22 22:47:19 +0700

    feat: cloudflare server side rendering for client

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
+import cloudflare from '@astrojs/cloudflare'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
@@ -7,6 +8,7 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
+	adapter: cloudflare(),
 	site: 'https://betterme.news/', // Write here your website url
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
