d96c51351895ca114c1cb0c85c99433cffa45d2f
LinuxDragon <linuxdragon@pop-os.localdomain>
2024-01-25 01:10:15 +0700

    feat: admin client get prod server link

--- admin_client/src/config.ts
+++ admin_client/src/config.ts
@@ -1,3 +1,5 @@
-const serverLink = 'http://localhost'
-const fetchLink = `${serverLink}/api`
+const isProd = process.env.DEV
+const devServerLink = 'http://localhost'
+const prodServerLink = 'https://api.betterme.news'
+const fetchLink = isProd ? `${prodServerLink}/api`: `${devServerLink}/api`
 export default fetchLink;
--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,4 +1,4 @@
-const isProd = import.meta.env.PROD;
+const isProd = import.meta.env.PROD
 const devServerLink = 'http://server:8080'
 const prodServerLink = 'https://api.betterme.news'
 const fetchLink = isProd ? `${prodServerLink}/api`: `${devServerLink}/api`
