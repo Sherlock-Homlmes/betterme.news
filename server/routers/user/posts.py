1d56f6c902d347dd7b056f8bbd7c55b8d59ff4a5
LinuxDragon <linuxdragon@pop-os.localdomain>
2024-02-19 22:42:44 +0700

    feat: api to get related posts

--- server/routers/user/posts.py
+++ server/routers/user/posts.py
@@ -6,6 +6,7 @@ from fastapi import APIRouter, HTTPException, Depends, BackgroundTasks
 from fastapi.responses import JSONResponse
 from beanie import PydanticObjectId
 from beanie.operators import ElemMatch
+from pydantic_core._pydantic_core import ValidationError
 
 # TODO: to error handler
 
@@ -91,7 +92,34 @@ async def get_post(
         if params.increase_view:
             background_tasks.add_task(post.increase_view)
         return post
-    except AttributeError:
+    except (AttributeError, ValidationError):
+        raise HTTPException(
+            status_code=ResponseStatusEnum.NOT_FOUND.value,
+            detail="Post not found",
+        )
+
+
+@router.get(
+    "/posts/{post_name}/_related",
+    tags=["Post"],
+    status_code=ResponseStatusEnum.OK.value,
+)
+async def get_related_post(
+    post_name: str,
+) -> List[GetPostListResponse]:
+    post_id: str = post_name.split("_")[-1]
+    try:
+        post = await Posts.get(post_id)
+        # TODO: only get not expired posts(when lib support datetime.date)
+        posts = await (
+            Posts
+            # find post that have same tags
+            .find(ElemMatch(Posts.tags, {"$in": post.tags}))
+            .aggregate([{"$sample": {"size": 3}}], projection_model=PostListProject)
+            .to_list()
+        )
+        return [post.model_dump(mode="json") for post in posts]
+    except (AttributeError, ValidationError):
         raise HTTPException(
             status_code=ResponseStatusEnum.NOT_FOUND.value,
             detail="Post not found",
