27e6b631971f5a6ce667e45350001172ca2f4edc
LinuxDragon <linuxdragon@pop-os.localdomain>
2024-06-09 21:07:11 +0700

    feat: demo page noindex nofollow

--- others/create_sample_env.sh
+++ others/create_sample_env.sh
@@ -3,6 +3,10 @@ if [ ! -f ./server/.env ]; then
     echo "Generate server .env file"
     cp server/example.env server/.env
 fi
+if [ ! -f ./client/.env ]; then
+    echo "Generate client .env file"
+    cp client/example.env client/.env
+fi
 if [ ! -f ./admin_client/.env ]; then
     echo "Generate admin_client .env file"
     cp admin_client/example.env admin_client/.env
