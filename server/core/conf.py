7cf9fa7883ff3ed8212ad7140dacc628357b3406
LinuxDragon <linuxdragon@pop-os.localdomain>
2024-06-09 19:54:56 +0700

    fix: allow origin

--- server/core/conf.py
+++ server/core/conf.py
@@ -83,7 +83,7 @@ allow_origins = (
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=allow_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
