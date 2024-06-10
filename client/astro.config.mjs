f0c81f2b6f00a5ce03361cf2fc36f105816a1260
LinuxDragon <linuxdragon@pop-os.localdomain>
2024-06-11 00:32:19 +0700

    fix: astro partytown not found + page title wrong

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,21 +1,24 @@
-import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import cloudflare from '@astrojs/cloudflare'
+import tailwind from '@astrojs/tailwind'
 
-import partytown from "@astrojs/partytown";
+// import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
-  site: 'https://betterme.news/',
-  // Write here your website url
-  markdown: {
-    drafts: true,
-    shikiConfig: {
-      theme: 'material-theme-palenight',
-      wrap: true
-    }
-  },
-  integrations: [tailwind(), partytown()]
-});
+	output: 'server',
+	adapter: cloudflare(),
+	site: 'https://betterme.news/',
+	// Write here your website url
+	markdown: {
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true,
+		},
+	},
+	integrations: [
+		tailwind(),
+		// partytown()
+	],
+})
