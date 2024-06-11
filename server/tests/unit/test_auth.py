bae8870d484f523c8f21f73da928c91fc7a8d80a
LinuxDragon <linuxdragon@pop-os.localdomain>
2024-06-12 00:43:26 +0700

    feat: add get oauth link api test

--- server/tests/unit/test_auth.py
+++ server/tests/unit/test_auth.py
@@ -6,10 +6,25 @@ import pytest
 async def test_get_self_fail_because_not_logged_in(client):
     """
     INPUT:
-        Get self api
+        Call self api
     OUTPUT:
-        Error not logged in
+        Error not authenticated
     """
     response = client.get("/api/auth/self")
     assert response.status_code == 403
     assert response.json()["detail"] == "Not authenticated"
+
+
+@pytest.mark.asyncio
+async def test_get_oauth_link(client):
+    """
+    INPUT:
+        Call oauth link api
+    OUTPUT:
+        Get discord link
+    """
+    response = client.get("/api/auth/oauth-link")
+    assert response.status_code == 200
+    assert (
+        response.json()["discord_link"] == "https://discord.com/api/oauth2/xxxxxxxxxxxxxxxxxxxxxx"
+    )
